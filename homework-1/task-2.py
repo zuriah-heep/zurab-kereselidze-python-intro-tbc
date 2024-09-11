47d96eede6f27b4de0a3017111d97173d3967782
zuriah-heep <zurab.kereselidze@gmail.com>
2024-09-11 12:43:43 +0400

    homework 1

---
+++ homework-1/task-2.py
@@ -0,0 +1,4 @@
+a = input("Enter your name: ")
+b = int(input("Enter the year of your birth: "))
+
+print("Hello", a + ',', "your age is", str(2024 - b) + '.')
---
+++ homework-1/task-4.py
@@ -0,0 +1,4 @@
+c = float(input("Enter temperature in Celsius: "))
+f = c * 9/5 + 32
+
+print("The temperature is", f, "Fahrenheit.")
