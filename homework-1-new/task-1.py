4943c520a2f5a31056e24303307d0a68097ff6a7
zuriah-heep <zurab.kereselidze@gmail.com>
2024-09-24 23:57:40 +0400

    homework-5

--- homework-1-new/task-1.py
+++
@@ -1,5 +0,0 @@
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-age = input("Enter your age: ")
-
-print("Hello", first_name, last_name + ',', "your age is", age + '.')
