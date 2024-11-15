44fc8eb70fd558bf80d5278b11f1399677fd2bac
zuriah-heep <zurab.kereselidze@gmail.com>
2024-11-15 21:46:09 +0400

    corrected task_2

--- homework-13/task_2.py
+++ homework-13/task_2.py
@@ -1,8 +1,9 @@
+from random import randint
+
+
 def main():
-    from random import randint
     list_1, list_2, list_3 = [[randint(0,100) for _ in range(30)] for _ in range(3)]
     print(list(map(lambda x, y, z: x + y + z, list_1, list_2, list_3)))
-
 
 if __name__ == "__main__":
     main()
