590063a2da8207e9c2d2b8538d079ab907adc19f
zuriah-heep <zurab.kereselidze@gmail.com>
2024-11-21 00:22:04 +0400

    homework-16

--- homework-16/admin.py
+++ homework-16/admin.py
@@ -19,7 +19,7 @@ def greetings():
 def add_session():
     print("Adding session")
     print("Enter the session details")
-    film_name = input("Film name: ")
+    film_name = input("Film name: ").strip()
     start_time = input("Start time: ")
     room_number = input("Room number: ")
     room_length = int(input("Room length: "))
@@ -39,28 +39,13 @@ def add_session():
     }
     print("Session added successfully")
 
-def list_sessions():
-    print("Listing sessions")
-    if not sessions:
-        print("No sessions found")
-        return
-
-    for key, val in sessions.items():
-        print(f"\tSession ID: {key}")
-        print(f"\tFilm name: {val['film_name']}")
-        print(f"\tStart time: {val['start_time']}")
-        print(f"\tRoom number: {val['room_number']}")
-        print(f"\tRoom length: {val['room_length']}")
-        print(f"\tRoom width: {val['room_width']}")
-        print(f"\tCapacity: {val['capacity']}")
-        print("\n")
-
 def remove_sessions():
     print("Removing session")
     if not sessions:
         print("No sessions found")
         return
 
+    print(f"Available sessions: {', '.join(sessions.keys())}")
     session_id = input("Session ID: ")
     if session_id in sessions:
         del sessions[session_id]
@@ -74,6 +59,7 @@ def edit_session():
         print("No sessions found")
         return
 
+    print(f"Available sessions: {', '.join(sessions.keys())}")
     session_id = input("Session ID: ")
     if session_id in sessions:
         print(f"Editing session {session_id}")
@@ -94,27 +80,3 @@ def edit_session():
         print("Session edited successfully")
     else:
         print(f"Session {session_id} not found.\n")
-
-def admin_loop():
-    greetings()
-    while True:
-        user_input = list_admin_menu_items()
-        match user_input:
-            case "1":
-                list_sessions()
-            case "2":
-                remove_sessions()
-            case "3":
-                add_session()
-            case "4":
-                edit_session()
-            case "5":
-                print("User menu")
-                print("1. list sessions")
-                print("2. search movie")
-                print("3. my tickets")
-                print("4. admin")
-                return process_user_input()
-            case _:
-                print("Invalid input")
-
--- homework-16/common.py
+++ homework-16/common.py
@@ -1,3 +1,5 @@
+from db import sessions
+
 def process_user_input():
     user_input = input("->: ").lower()
     if user_input not in ["1", "2", "3", "4", "exit"]:
@@ -17,3 +19,19 @@ def process_admin_input():
         print("Goodbye")
         exit()
     return user_input
+
+def list_sessions():
+    print("Listing sessions")
+    if not sessions:
+        print("No sessions found")
+        return
+
+    for key, val in sessions.items():
+        print(f"\tSession ID: {key}")
+        print(f"\tFilm name: {val['film_name']}")
+        print(f"\tStart time: {val['start_time']}")
+        print(f"\tRoom number: {val['room_number']}")
+        print(f"\tRoom length: {val['room_length']}")
+        print(f"\tRoom width: {val['room_width']}")
+        print(f"\tCapacity: {val['capacity']}")
+        print("\n")
